db740ae5a7bc11e540575f3ed565519bd9496a2a
lateef-taiwo <taiwolateef55@gmail.com>
2025-08-02 16:24:36 -0500

    modified:   modules/key/output.tf

--- modules/key/output.tf
+++ modules/key/output.tf
@@ -2,6 +2,11 @@
 #   value = aws_key_pair.client_key.key_name
 # }
 
+output "key_name" {
+  description = "The name of the key pair"
+  value       = aws_key_pair.client_key.key_name
+}
+
 output "ssh_key_value" {
   value     = data.aws_secretsmanager_secret_version.server_key_pub.secret_string
   sensitive = true
