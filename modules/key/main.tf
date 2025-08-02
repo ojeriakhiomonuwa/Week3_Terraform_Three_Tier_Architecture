8ce2f274baa1d22cfeaa5e8f07ebd8bad5e6489d
Lateef Taiwo <taiwolateef55@gmail.com>
2025-08-02 01:47:04 -0500

    modified:   modules/key/main.tf

--- modules/key/main.tf
+++ modules/key/main.tf
@@ -1,4 +1,13 @@
+# resource "aws_key_pair" "client_key" {
+#     key_name = "server_key"
+#     public_key = file("../modules/key/server_key.pub")
+# }
+
+data "aws_secretsmanager_secret_version" "ssh_key" {
+  secret_id = "my-ssh-public-key"
+}
+
 resource "aws_key_pair" "client_key" {
-    key_name = "server_key"
-    public_key = file("../modules/key/server_key.pub")
+  key_name   = 3-tier_server_public_key
+  public_key = data.aws_secretsmanager_secret_version.ssh_key.secret_string
 }
